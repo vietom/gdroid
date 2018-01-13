1a7176dcec23b2533a829573d51b4536f0c5f6dc
Thomas Prochaska <thomas@tlt.local>
2018-01-13 16:55:43 +0400

    factored out the emulator communication

--- cmd/comm.go
+++ cmd/comm.go
@@ -1,1 +1,91 @@
 package cmd
+
+import (
+	"fmt"
+	"strings"
+	"strconv"
+	"net"
+	"bufio"
+	"io/ioutil"
+)
+
+func discardUntilOK(rw *bufio.ReadWriter) error {
+	for {
+		line, err := rw.ReadString('\n')
+		if err != nil {
+			return err
+		}
+		if line == "OK\r\n" {
+			return nil
+		}
+	}
+}
+
+func read(rw *bufio.ReadWriter) (string, error) {
+	line, err := rw.ReadString('\n')
+	if err != nil {
+		return "", err
+	}
+	line = strings.TrimSpace(line)
+	return line, nil
+}
+
+func write(rw *bufio.ReadWriter, cmd string) error {
+		s := fmt.Sprintf("%s\r\n", cmd)
+		_, err := rw.WriteString(s)
+		if err != nil {
+			return err
+		}
+		rw.Flush()
+		return nil
+}
+
+
+func GetAVDNameForDevice(device string) (string, error) {
+		// fetch key needed for the emulator telnet connection
+		b, err := ioutil.ReadFile(emulator_auth_token_file) // just pass the file name
+		if err != nil {
+			return "", err
+		}
+		key := strings.TrimSpace(string(b))
+
+		// open the telnet connection
+		port, err := strconv.Atoi(strings.Split(device, "-")[1])
+		if err != nil {
+			return "", err
+		}
+		address := fmt.Sprintf("localhost:%d", port)
+		conn, err := net.Dial("tcp", address)
+		if err != nil {
+			return "", err
+		}
+		rw := bufio.NewReadWriter(bufio.NewReader(conn), bufio.NewWriter(conn))
+		defer conn.Close()
+
+		// authenticate against the device
+		err = discardUntilOK(rw)
+		if err != nil {
+			return "", err
+		}
+		auth_cmd := fmt.Sprintf("auth %s", key)
+		err = write(rw, auth_cmd)
+		if err != nil {
+			return "", err
+		}
+
+		// fetch avd name
+		err = discardUntilOK(rw)
+		if err != nil {
+			return "", err
+		}
+		err = write(rw, "avd name")
+		if err != nil {
+			return "", err
+		}
+		name, err := read(rw)
+		if err != nil {
+			return "", err
+		}
+
+		return name, nil
+}
--- cmd/start.go
+++ cmd/start.go
@@ -1,13 +1,7 @@
 package cmd
 
 import (
-	"fmt"
-	"net"
-	"bufio"
-	"io/ioutil"
 	"os/exec"
-	"strings"
-	"strconv"
 	"github.com/spf13/cobra"
 	"github.com/nhooyr/color"
 )
@@ -21,81 +15,9 @@ func isAvailable(available_avds []string, requested_avd string) bool {
 	return false
 }
 
-func readUntilOK(rw *bufio.ReadWriter) error {
-	for {
-		line, err := rw.ReadString('\n')
-		if err != nil {
-			return err
-		}
-		if line == "OK\r\n" {
-			return nil
-		}
-	}
-}
-
-func read(rw *bufio.ReadWriter) (string, error) {
-	line, err := rw.ReadString('\n')
-	if err != nil {
-		return "", err
-	}
-	line = strings.TrimSpace(line)
-	return line, nil
-}
-
-func write(rw *bufio.ReadWriter, cmd string) error {
-		s := fmt.Sprintf("%s\r\n", cmd)
-		_, err := rw.WriteString(s)
-		if err != nil {
-			return err
-		}
-		rw.Flush()
-		return nil
-}
-
 func isRunning(running_emulated_devices []string, requested_avd string) bool {
 	for _, device := range running_emulated_devices {
-		port, err := strconv.Atoi(strings.Split(device, "-")[1])
-		if err != nil {
-			continue
-		}
-
-		address := fmt.Sprintf("localhost:%d", port)
-		conn, err := net.Dial("tcp", address)
-		if err != nil {
-			color.Printf("%h[fgRed]net error%r\n")
-			continue
-		}
-		// fetch key needed for the emulator telnet connection
-		b, err := ioutil.ReadFile(emulator_auth_token_file) // just pass the file name
-		if err != nil {
-			color.Printf("%h[fgRed]%s%r\n", err)
-			continue
-		}
-		key := string(b)
-		auth_cmd := fmt.Sprintf("auth %s", key)
-		rw := bufio.NewReadWriter(bufio.NewReader(conn), bufio.NewWriter(conn))
-		defer conn.Close()
-		err = readUntilOK(rw)
-		if err != nil {
-			color.Printf("%h[fgRed]%s%r\n", err)
-			continue
-		}
-		err = write(rw, auth_cmd)
-		if err != nil {
-			color.Printf("%h[fgRed]net error%r\n")
-			continue
-		}
-		err = readUntilOK(rw)
-		if err != nil {
-			color.Printf("%h[fgRed]%s%r\n", err)
-			continue
-		}
-		err = write(rw, "avd name")
-		if err != nil {
-			color.Printf("%h[fgRed]net error%r\n")
-			continue
-		}
-		name, err := read(rw)
+		name, err := GetAVDNameForDevice(device)
 		if err != nil {
 			color.Printf("%h[fgRed]%s%r\n", err)
 			continue
